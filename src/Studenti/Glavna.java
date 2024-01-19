9b5116c7766490fd70aad97bd2b3ecb56050420f
SimonaR07 <sristomanova32@gmail.com>
2024-01-19 21:07:28 +0100

    Add new java program

---
+++ src/Studenti/Glavna.java
@@ -0,0 +1,15 @@
+package Studenti;
+
+public class Glavna {
+	public static void main(String[] args) {
+		Student marko = new Student ();
+		
+		marko.setId(1);
+		marko.setIme("Marko");
+		marko.setGodini(18);
+		
+		System.out.println("Reden broj "+marko.getId()+" e studentot "+marko.getIme()+" star "+marko.getGodini()+" godini.");
+		
+	}
+
+}
